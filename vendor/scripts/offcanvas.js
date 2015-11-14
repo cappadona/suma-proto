272941f68c986844c26f8c6964e5179c47884d95
Nick Cappadona <nickypasta@gmail.com>
2015-11-14 11:58:42 -0600

    Toggle sidemenu icon for active/inactive

--- vendor/scripts/offcanvas.js
+++ vendor/scripts/offcanvas.js
@@ -5,9 +5,22 @@ var $ = require('jquery');
 
   var toggleSidemenu = function () {
     $('.offcanvas, .close-canvas').toggleClass('active');
+    toggleIcon($('.offcanvas').hasClass('active'));
   }
 
-  // Toggle Nav on Click
+  // Toggle icon for side menu button
+  var toggleIcon = function (active) {
+    if (active) {
+      $('#slider').removeClass('fa-angle-double-right');
+      $('#slider').addClass('fa-angle-double-left');
+    }
+    else {
+      $('#slider').removeClass('fa-angle-double-left');
+      $('#slider').addClass('fa-angle-double-right');
+    }
+  }
+
+  // Toggle side menu on click
   $(document).ready(function() {
     $('.toggle-sidemenu, .close-canvas').click(function() {
       toggleSidemenu();
