c73f949dbc53dfaed5c00ab509ed4e929df94df4
Nick Cappadona <nickypasta@gmail.com>
2015-11-14 09:08:29 -0600

    Overhaul to offcanvas
    Would be nice to utilize autoprefixer for production.

--- vendor/scripts/offcanvas.js
+++ vendor/scripts/offcanvas.js
@@ -1,7 +1,23 @@
 var $ = require('jquery');
 
-$(document).ready(function() {
-  $('[data-toggle="offcanvas"]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
+(function (document, window, undefined) {
+  'use strict';
+
+  var toggleSidemenu = function () {
+    if ($('.offcanvas').hasClass('offcanvas-active')) {
+      // Close sidemenu
+      $('.offcanvas').removeClass('offcanvas-active');
+      $('.close-canvas').removeClass('close-canvas-active');
+    } else {
+      // Openn sidemenu
+      $('.offcanvas').addClass('offcanvas-active');
+      $('.close-canvas').addClass('close-canvas-active');
+    }
+  }
+
+  // Toggle Nav on Click
+  $('.toggle-sidemenu, .close-canvas').click(function() {
+    toggleSidemenu();
   });
-});
+
+})(document, window);
